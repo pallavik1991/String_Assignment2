842dc4e6309781ab710935003ecc94ac0df4730d
pallavik1991 <pallu.katti@gmail.com>
2017-03-05 03:25:09 -0800

    Add files via upload

---
+++ Assignment7_2.java
@@ -0,0 +1,18 @@
+class Assignment7_2
+{
+	public static void main(String[] args)
+	{
+		String str1,str2;
+		str1="Hello Good Morning 123";
+		str2="Good";
+		
+		if(str1.contains(str2))
+                {
+                    System.out.println("Substring");
+                }
+                else
+                {
+                    System.out.println("Not substring");
+                }
+	}
+}
